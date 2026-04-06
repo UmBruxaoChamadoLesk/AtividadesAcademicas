c33f52f4832c4a01caebbddb3b3f31714ffd1338
UmBruxaoChamadoLesk <lukegalvao123@gmail.com>
2026-04-05 22:59:56 -0300

    questões de prova-1 sendo feitas

---
+++ ES_ARQUIVOS/P1_2025-1/questão-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct registroTemporalCovid19
+{
+    int dia;
+    int novos_casos;
+    int obitos;
+} typedef tempCovid;
+
+
+int main(int argc, char **argv)
+{
+    FILE *file = fopen(argv[1],"rb");
+
+        if(file == NULL)
+        {
+            printf("arquivo nao existe\n");
+            return 1;
+        }
+
+    fseek(file,0,SEEK_END);
+    int tamanhoArq = ftell(file);
+    int qtdReg = (tamanhoArq/sizeof(tempCovid));
+    rewind(file);
+
+    printf("tamanho do arquivo em bytes:%d\ntamanho do registro em bytes:%d\nquantidade de registros:%d\n",tamanhoArq,sizeof(tempCovid),qtdReg);
+
+    fclose(file);
+
+    return 0;
+
+
+
+}
